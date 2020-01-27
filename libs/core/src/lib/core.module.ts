53825bc1d9b56a477ce0298b0ee3f1d1e896e6d9
Alex Malkevich <alex.malkevich@spryker.com>
2020-01-27 15:44:37 +0100

    feat(core): scaffold lib with build target

---
+++ libs/core/src/lib/core.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+/**
+ * @public
+ */
+@NgModule({
+  imports: [CommonModule]
+})
+export class SpyCoreModule {}

98b281d2a9b3a6a2a9bc6a2bafecdad13acb2012
Alex Malkevich <alex.malkevich@spryker.com>
2020-02-06 12:55:21 +0100

    fix(styles): enable partial styles for components
    And support global styles on the shadow dom side

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,2 +1,2 @@
 // Global Ant Design styles
-import '!style-loader!css-loader!less-loader?javascriptEnabled=true!../libs/styles/src/lib/themes/default-webpack.less';
+import '!style-loader!css-loader!less-loader?javascriptEnabled=true!../libs/styles/src/lib/themes/default/standalone-webpack.less';
--- libs/core/src/lib/test.component.ts
+++ libs/core/src/lib/test.component.ts
@@ -1,9 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'spy-test',
   template: `
     <input nz-input value="test" />
-  `
+  `,
+  styleUrls: ['./test.component.less', './test.component.scss'],
+  encapsulation: ViewEncapsulation.ShadowDom
 })
 export class TestComponent {}

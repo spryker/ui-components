dfe811dc1365a99f538a0887ebee06b7050e8350
Aleksey Belan <alexey.belan@spryker.com>
2020-02-06 18:26:34 +0200

    style: cleanup diffs

--- libs/core/src/lib/test.component.ts
+++ libs/core/src/lib/test.component.ts
@@ -3,7 +3,7 @@ import { Component, ViewEncapsulation } from '@angular/core';
 @Component({
   selector: 'spy-test',
   template: `
-      <h1>semibbold</h1>
+    <h1>semibbold</h1>
     <input nz-input value="test" />
   `,
   styleUrls: ['./test.component.less', './test.component.scss'],

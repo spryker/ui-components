d586179c8b3563c1ae14a1e310d28852d4fbc2b7
Alex Malkevich <alex.malkevich@spryker.com>
2020-02-13 15:56:22 +0100

    style(format): apply

--- libs/utils/src/index.ts
+++ libs/utils/src/index.ts
@@ -3,4 +3,3 @@ export * from './lib/apply-attrs/apply-attrs.module';
 export * from './lib/apply-attrs/apply-attrs.directive';
 export * from './lib/of-type.pipe';
 export * from './lib/of-type.module';
-

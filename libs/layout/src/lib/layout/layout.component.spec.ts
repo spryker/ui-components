6712b261b7f4cffb91e18d07cab4128438cb3472
galitsky <eugen.galitsky@spryker.com>
2020-02-17 17:03:47 +0200

    feat(layout): layout component basic

---
+++ libs/layout/src/lib/layout/layout.component.spec.ts
@@ -0,0 +1,45 @@
+import { Component, NO_ERRORS_SCHEMA } from '@angular/core';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { LayoutModule } from '../layout.module';
+import { LayoutComponent } from './layout.component';
+
+describe('LayoutComponent', () => {
+  let component: TestComponent;
+  let fixture: ComponentFixture<TestComponent>;
+
+  @Component({
+    template: `
+      <spy-layout>
+        <div class="test-content">Header Content</div>
+      </spy-layout>
+    `,
+  })
+  class TestComponent {}
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      imports: [LayoutModule],
+      declarations: [TestComponent],
+      schemas: [NO_ERRORS_SCHEMA],
+    }).compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(TestComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should render content', () => {
+    const headerContentElement = fixture.debugElement.query(
+      By.css('.test-content'),
+    );
+
+    expect(headerContentElement).toBeTruthy();
+  });
+});

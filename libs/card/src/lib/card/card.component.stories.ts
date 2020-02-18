8ad630dc426412e30ba170673b066ea75e3a6894
Aleksey Belan <alexey.belan@spryker.com>
2020-02-18 15:37:40 +0200

    improvement(logo): add icon cssurl

--- libs/card/src/lib/card/card.component.stories.ts
+++ libs/card/src/lib/card/card.component.stories.ts
@@ -2,6 +2,7 @@ import { NzCardModule } from 'ng-zorro-antd/card';
 import { CardComponent } from './card.component';
 import { CardModule } from '../card.module';
 import { ButtonModule } from '@spryker/button';
+import { LogoModule } from '@spryker/logo';
 
 export default {
   title: 'CardComponent',
@@ -9,9 +10,12 @@ export default {
 
 export const primary = () => ({
   moduleMetadata: {
-    imports: [NzCardModule, CardModule, ButtonModule],
+    imports: [NzCardModule, CardModule, ButtonModule, LogoModule],
   },
   template: `
+    <div style="width: 190px; height: 80px;">
+        <spy-logo></spy-logo>
+    </div>
     <spy-card title="Card Title" [extra]="extraRef" [actions]="[button, button]">
       Card content here
       <spy-button>Button Content</spy-button>
--- libs/utils/src/lib/to-json.ts
+++ libs/utils/src/lib/to-json.ts
@@ -1,20 +1,20 @@
 export function ToJson() {
-    return function (target: any, key: string) {
-        const newKey = '__json-value';
+  return function(target: any, key: string) {
+    const newKey = '__json-value';
 
-        const getter = function(this: any) {
-            return this[newKey];
-        };
+    const getter = function(this: any) {
+      return this[newKey];
+    };
 
-        const setter = function(this: any, prop: Record<string, string> | string) {
-            this[newKey] = typeof prop === 'string' ? JSON.parse(prop) : prop;
-        };
+    const setter = function(this: any, prop: Record<string, string> | string) {
+      this[newKey] = typeof prop === 'string' ? JSON.parse(prop) : prop;
+    };
 
-        Object.defineProperty(target, key, {
-            get: getter,
-            set: setter,
-            enumerable: true,
-            configurable: false
-        });
-    }
+    Object.defineProperty(target, key, {
+      get: getter,
+      set: setter,
+      enumerable: true,
+      configurable: false,
+    });
+  };
 }

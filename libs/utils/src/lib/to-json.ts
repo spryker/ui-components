6abd5dd8a50032916df8b483842be51c19828708
Aleksey Belan <alexey.belan@spryker.com>
2020-02-18 16:01:11 +0200

    style: fix style

--- libs/utils/src/lib/to-json.ts
+++ libs/utils/src/lib/to-json.ts
@@ -1,20 +1,18 @@
-export const ToJson = () => (
-  (target: any, key: string) => {
-    const newKey = '__json-value';
+export const ToJson = () => (target: any, key: string) => {
+  const newKey = '__json-value';
 
-    const getter = function(this: any) {
-      return this[newKey];
-    };
+  const getter = function(this: any) {
+    return this[newKey];
+  };
 
-    const setter = function(this: any, prop: Record<string, string> | string) {
-      this[newKey] = typeof prop === 'string' ? JSON.parse(prop) : prop;
-    };
+  const setter = function(this: any, prop: Record<string, string> | string) {
+    this[newKey] = typeof prop === 'string' ? JSON.parse(prop) : prop;
+  };
 
-    Object.defineProperty(target, key, {
-      get: getter,
-      set: setter,
-      enumerable: true,
-      configurable: true,
-    });
-  }
-);
+  Object.defineProperty(target, key, {
+    get: getter,
+    set: setter,
+    enumerable: true,
+    configurable: true,
+  });
+};

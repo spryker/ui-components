6ac73d2ec70f1fd1578ddc77bdff92821b7aef92
Alex Malkevich <alex.malkevich@spryker.com>
2020-02-19 10:49:44 +0100

    refactor(utils): use function definition for ToJson decorator
    As it is statically analizeable for NGC

--- libs/utils/src/lib/to-json.ts
+++ libs/utils/src/lib/to-json.ts
@@ -1,18 +1,20 @@
-export const ToJson = () => (target: any, key: string) => {
-  const newKey = '__json-value';
+export function ToJson(): PropertyDecorator {
+  return (target, key) => {
+    const newKey = '__json-value';
 
-  const getter = function(this: any) {
-    return this[newKey];
+    const getter = function(this: any) {
+      return this[newKey];
+    };
+
+    const setter = function(this: any, prop: Record<string, string> | string) {
+      this[newKey] = typeof prop === 'string' ? JSON.parse(prop) : prop;
+    };
+
+    Object.defineProperty(target, key, {
+      get: getter,
+      set: setter,
+      enumerable: true,
+      configurable: true,
+    });
   };
-
-  const setter = function(this: any, prop: Record<string, string> | string) {
-    this[newKey] = typeof prop === 'string' ? JSON.parse(prop) : prop;
-  };
-
-  Object.defineProperty(target, key, {
-    get: getter,
-    set: setter,
-    enumerable: true,
-    configurable: true,
-  });
-};
+}

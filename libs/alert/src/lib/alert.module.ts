8fdf8854334bd385f659bef9b575937421b76d3e
Aleksey Belan <alexey.belan@spryker.com>
2020-02-24 16:45:35 +0200

    refactor(alert): remove icon provider

--- libs/alert/src/lib/alert.module.ts
+++ libs/alert/src/lib/alert.module.ts
@@ -3,29 +3,11 @@ import { CommonModule } from '@angular/common';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NzAlertModule } from 'ng-zorro-antd/alert';
 import { AlertComponent } from './alert/alert.component';
-import { errorIcon } from './alert/alert.component.icons';
-import { IconModule, ICONS_TOKEN } from '@spryker/icon';
-
-const icons = [
-  {
-    name: 'error',
-    svg: function() {
-      return new Promise(resolve => {
-        resolve(errorIcon);
-      });
-    },
-  },
-];
+import { IconModule } from '@spryker/icon';
 
 @NgModule({
   imports: [CommonModule, IconModule, BrowserAnimationsModule, NzAlertModule],
   declarations: [AlertComponent],
   exports: [AlertComponent],
-  providers: [
-    {
-      provide: ICONS_TOKEN,
-      useValue: icons,
-    },
-  ],
 })
 export class AlertModule {}

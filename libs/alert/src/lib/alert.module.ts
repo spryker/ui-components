5f676c1fcc866f977ad6d84a82f1cba0456d2fef
Aleksey Belan <alexey.belan@spryker.com>
2020-02-24 14:36:46 +0200

    feat(alert): add icon

--- libs/alert/src/lib/alert.module.ts
+++ libs/alert/src/lib/alert.module.ts
@@ -1,12 +1,31 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { NzAlertModule } from 'ng-zorro-antd/alert';
 import { AlertComponent } from './alert/alert.component';
-import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { errorIcon } from './alert/alert.component.icons';
+import { IconModule, ICONS_TOKEN } from '@spryker/icon';
+
+const icons = [
+  {
+    name: 'error',
+    svg: function() {
+      return new Promise(resolve => {
+        resolve(errorIcon);
+      });
+    },
+  }
+];
 
 @NgModule({
-  imports: [CommonModule, BrowserAnimationsModule, NzAlertModule],
+  imports: [CommonModule, IconModule, BrowserAnimationsModule, NzAlertModule],
   declarations: [AlertComponent],
   exports: [AlertComponent],
+  providers: [
+    {
+      provide: ICONS_TOKEN,
+      useValue: icons,
+    },
+  ],
 })
 export class AlertModule {}

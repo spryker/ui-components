bcb6a85644109700ab00b7332b84d6bb94f8511c
Dmytro Abramov <dmytro.abramov@spryker.com>
2020-02-25 17:12:27 +0200

    feat(textarea,input): textarea component, input styles

---
+++ libs/textarea/src/lib/textarea/textarea.component.stories.ts
@@ -0,0 +1,14 @@
+import { TextareaComponent } from './textarea.component';
+import { TextareaModule } from '../textarea.module';
+
+export default {
+  title: 'TextareaComponent',
+};
+
+export const primary = () => ({
+  moduleMetadata: {
+    imports: [TextareaModule],
+  },
+  component: TextareaComponent,
+  props: {},
+});

c4500f955d6e6f54ddcc5ab72502e6d1a18d3813
Dmytro Abramov <dmytro.abramov@spryker.com>
2020-02-26 18:18:57 +0200

    feat(toggle component): toggle component and styles

---
+++ libs/toggle/.storybook/config.js
@@ -0,0 +1,6 @@
+import '../../../.storybook/config';
+import { configure, addDecorator } from '@storybook/angular';
+import { withKnobs } from '@storybook/addon-knobs';
+
+addDecorator(withKnobs);
+configure(require.context('../src/lib', true, /\.stories\.tsx?$/), module);
---
+++ libs/toggle/src/lib/toggle.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ToggleComponent } from './toggle/toggle.component';
+import { NzSwitchModule } from 'ng-zorro-antd/switch';
+import { FormsModule } from '@angular/forms';
+
+@NgModule({
+  imports: [CommonModule, NzSwitchModule, FormsModule],
+  declarations: [ToggleComponent],
+  exports: [ToggleComponent],
+})
+export class ToggleModule {}

bda6768c2b14edcb92047e810b9a2688171b3f27
Dmytro Abramov <dmytro.abramov@spryker.com>
2020-02-26 18:18:57 +0200

    feat(toggle component): toggle component and styles

---
+++ libs/toggle/src/lib/toggle/toggle.component.stories.ts
@@ -0,0 +1,15 @@
+import { ToggleComponent } from './toggle.component';
+import { NzSwitchModule } from 'ng-zorro-antd/switch';
+import { ToggleModule } from '../toggle.module';
+
+export default {
+  title: 'ToggleComponent',
+};
+
+export const primary = () => ({
+  moduleMetadata: {
+    imports: [NzSwitchModule, ToggleModule],
+  },
+  component: ToggleComponent,
+  props: {},
+});

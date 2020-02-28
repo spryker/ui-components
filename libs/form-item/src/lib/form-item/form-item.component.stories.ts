e4f8455e71a938cfef3db34d33a853477cdf66c7
Aleksey Belan <alexey.belan@spryker.com>
2020-02-28 14:34:00 +0200

    fix(form-item): remove input from stories

--- libs/form-item/src/lib/form-item/form-item.component.stories.ts
+++ libs/form-item/src/lib/form-item/form-item.component.stories.ts
@@ -2,7 +2,6 @@ import { NzFormModule } from 'ng-zorro-antd/form';
 import { FormItemComponent } from './form-item.component';
 import { NzInputModule } from 'ng-zorro-antd/input';
 import { FormItemModule } from '@spryker/form-item';
-import { InputModule } from '@spryker/input';
 
 export default {
   title: 'FormItemComponent',
@@ -10,20 +9,17 @@ export default {
 
 export const primary = () => ({
   moduleMetadata: {
-    imports: [FormItemModule, NzFormModule, NzInputModule, InputModule],
+    imports: [FormItemModule, NzFormModule, NzInputModule],
   },
   template: `
     <spy-form-item for="input-id" error="Error Message">
       Label
-      <spy-input placeholder="placeholder" attrs='{"id":"form-id"}' type="text" control></spy-input>
     </spy-form-item>
    <spy-form-item for="input-id" error="Error Message">
       Label
-      <spy-input attrs='{"id":"form-id2"}' type="text" control></spy-input>
     </spy-form-item>
     <spy-form-item for="input-id" error="Error Message">
       Label
-      <spy-input attrs='{"id":"form-id3"}' type="text" control></spy-input>
     </spy-form-item>
   `,
 });

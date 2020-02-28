f5ff37f47535106a7b80211bcb9997483e61b3e8
Aleksey Belan <alexey.belan@spryker.com>
2020-02-28 15:15:13 +0200

    feat(checkbox): add storybook

--- libs/checkbox/src/lib/checkbox.module.ts
+++ libs/checkbox/src/lib/checkbox.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CheckboxComponent } from './checkbox/checkbox.component';
+import { NzCheckboxModule } from 'ng-zorro-antd/checkbox';
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [CommonModule, NzCheckboxModule],
   declarations: [CheckboxComponent],
   exports: [CheckboxComponent],
 })

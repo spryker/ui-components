0d651a75e3553eb702f56c12dcb9d951a5a8a232
gund <malkevich.alex@gmail.com>
2020-03-02 02:37:50 +0100

    chore(storybook): add global support for custom elements in storybooks

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,2 +1,4 @@
 // Global Ant Design styles
 import '!style-loader!css-loader!less-loader?javascriptEnabled=true!../libs/styles/src/lib/themes/default/standalone-webpack.less';
+
+import '@webcomponents/webcomponentsjs/custom-elements-es5-adapter.js';

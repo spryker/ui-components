5732ea645d3b49d481fd8c737ac9c48ee4a2376c
gund <malkevich.alex@gmail.com>
2020-03-02 02:36:10 +0100

    feat(web-components): add select components directive

--- libs/web-components/src/index.ts
+++ libs/web-components/src/index.ts
@@ -1,3 +1,4 @@
 export * from './lib/web-components.module';
 export * from './lib/custom-element';
 export * from './lib/ng-web-component';
+export * from './lib/select-components';
---
+++ libs/web-components/src/lib/select-components/select-components.directive.spec.ts
@@ -0,0 +1,12 @@
+import { TestBed, async } from '@angular/core/testing';
+import { SelectComponentsDirective } from './select-components.directive';
+
+xdescribe('Directive: SelectComponents', () => {
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [SelectComponentsDirective],
+    }).compileComponents();
+  }));
+
+  it('should create an instance', () => {});
+});

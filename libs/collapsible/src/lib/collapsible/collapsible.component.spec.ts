216a544826c97c78e0a2d38e984f34a0a625db0b
Alex Malkevich <malkevich.alex@gmail.com>
2020-03-10 20:50:45 +0100

    test(icons): fix remaining tests of icons

--- libs/collapsible/src/lib/collapsible/collapsible.component.spec.ts
+++ libs/collapsible/src/lib/collapsible/collapsible.component.spec.ts
@@ -2,23 +2,9 @@ import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
-import { IconService, provideIcons } from '@spryker/icon';
 import { NzCollapseModule } from 'ng-zorro-antd/collapse';
 
 import { CollapsibleComponent } from './collapsible.component';
-
-const iconArrow = `<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 10 10">
-    <defs>
-        <path d="M5.122 9.146a1.264 1.264 0 01-.032-.033L1.389 5.274a1.36 1.36 0 010-1.887 1.264 1.264 0 011.82 0L6 6.282l2.792-2.895a1.264 1.264 0 011.786-.033l.033.033a1.36 1.36 0 010 1.887l-3.7 3.839a1.258 1.258 0 01-.952.386 1.259 1.259 0 01-.837-.353z" id="a"/>
-    </defs>
-    <g transform="matrix(0 1 1 0 -1 -1)" fill="none" fill-rule="evenodd">
-        <mask id="b" fill="#fff"><use xlink:href="#a"/></mask>
-        <use fill="currentColor" xlink:href="#a"/>
-        <g mask="url(#b)" fill="currentColor">
-            <path d="M.6.3h10.8v10.8H.6z"/>
-        </g>
-    </g>
-</svg>`;
 
 describe('CollapsibleComponent', () => {
   let component: CollapsibleComponent;
@@ -29,22 +15,14 @@ describe('CollapsibleComponent', () => {
       imports: [NzCollapseModule, NoopAnimationsModule],
       declarations: [CollapsibleComponent],
       schemas: [NO_ERRORS_SCHEMA],
-      providers: [
-        IconService,
-        provideIcons([
-          {
-            name: 'arrow',
-            svg: iconArrow,
-          },
-        ]),
-      ],
+      providers: [],
     }).compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(CollapsibleComponent);
     component = fixture.componentInstance;
-    component.titleIcon = 'arrow';
+    component.titleIcon = 'title-icon';
     fixture.detectChanges();
   });
 
@@ -67,6 +45,7 @@ describe('CollapsibleComponent', () => {
     const headerIcon = collapsibleHeaderElem.query(By.css('spy-icon'));
 
     expect(headerIcon).toBeTruthy();
+    expect(headerIcon.properties.name).toBe('title-icon');
   });
 
   describe('Toggling functionality', () => {

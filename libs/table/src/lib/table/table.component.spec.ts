55ca5021f96b2f8664027093d21b7316e505d15c
Aleksey Belan <alexey.belan@spryker.com>
2020-03-11 10:36:50 +0200

    improvement(table): add test http structure

--- libs/table/src/lib/table/table.component.spec.ts
+++ libs/table/src/lib/table/table.component.spec.ts
@@ -2,27 +2,81 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 
 import { TableComponent } from './table.component';
-import { NO_ERRORS_SCHEMA } from "@angular/core";
+import { Component, NO_ERRORS_SCHEMA } from "@angular/core";
+import { By } from "@angular/platform-browser";
 
 describe('TableComponent', () => {
-  let component: TableComponent;
-  let fixture: ComponentFixture<TableComponent>;
+  let component: TestComponent;
+  let fixture: ComponentFixture<TestComponent>;
+  let httpTestingController: HttpTestingController;
+
+  @Component({
+    selector: 'test-component',
+    template: `<spy-table [config]="config"></spy-table>`
+  })
+  class TestComponent {
+    config: any;
+  }
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [TableComponent],
+      declarations: [TableComponent, TestComponent],
       imports: [HttpClientTestingModule],
       schemas: [NO_ERRORS_SCHEMA],
     }).compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(TableComponent);
+    fixture = TestBed.createComponent(TestComponent);
     component = fixture.componentInstance;
+    httpTestingController = TestBed.get(HttpTestingController);
+  });
+
+  afterEach(() => {
+    httpTestingController.verify();
   });
 
   it('should create', () => {
-    expect(component).toBeTruthy();
+    component.config = {
+      dataUrl: 'https://angular-recipe-24caa.firebaseio.com/data.json',
+      colsUrl: 'https://angular-recipe-24caa.firebaseio.com/col.json',
+      selectable: true,
+    };
+
+    const mockedCols = [
+      {
+        id: 'name',
+        title: 'name',
+        sortable: true,
+        width: '40%'
+      },
+      {
+        id: 'sku',
+        title: 'sku',
+        sortable: true
+      },
+      {
+        id: 'id3',
+        title: 'id3',
+        sortable: true
+      }
+    ];
+
+    fixture.detectChanges();
+
+    const req = httpTestingController.expectOne('https://angular-recipe-24caa.firebaseio.com/col.json');
+    const req2 = httpTestingController.expectOne('https://angular-recipe-24caa.firebaseio.com/data.json?page=0');
+
+    expect(req.request.method).toEqual('GET');
+    req.flush(mockedCols);
+
+    fixture.detectChanges();
+
+    const labelElem = fixture.debugElement.queryAll(
+      By.css('tr th'),
+    );
+
+    console.log(labelElem);
   });
 
   describe('Template structure', () => {

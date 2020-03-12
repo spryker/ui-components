e721474dba26496bc634ccf8a77151c8d36d39a0
Dmytro Abramov <dmytro.abramov@spryker.com>
2020-03-12 19:03:47 +0200

    refactor(dropdown): added toboolean

--- libs/dropdown/src/lib/dropdown/dropdown.component.ts
+++ libs/dropdown/src/lib/dropdown/dropdown.component.ts
@@ -6,6 +6,7 @@ import {
   ViewEncapsulation,
   ChangeDetectionStrategy,
 } from '@angular/core';
+import { ToBoolean } from '@spryker/utils';
 
 export interface DropdownItem {
   action: string;
@@ -32,8 +33,8 @@ export type Placement =
 export class DropdownComponent {
   @Input() items: DropdownItem[] = [];
   @Input() placement: Placement = 'bottomRight';
-  @Input() visible = false;
-  @Input() disabled = false;
+  @Input() @ToBoolean() visible = false;
+  @Input() @ToBoolean() disabled = false;
   @Output() visibleChange = new EventEmitter<boolean>();
   @Output() actionTriggered = new EventEmitter<string>();
 }

2e9a3b96fd270e715ff0621bb8bded6c5c25c1d4
galitsky <eugen.galitsky@spryker.com>
2020-03-13 19:26:52 +0200

    fix(button): toboolean decorator

--- libs/button/src/lib/button/button.component.ts
+++ libs/button/src/lib/button/button.component.ts
@@ -43,7 +43,7 @@ export class ButtonComponent implements OnChanges {
   @Input() type: Props['type'] = 'button';
   @Input() shape: Props['shape'] = 'default';
   @Input() size: Props['size'] = 'md';
-  @Input() disabled: Props['disabled'] = false;
+  @Input() @ToBoolean() disabled: Props['disabled'] = false;
   @Input() variant: Props['variant'] = 'primary';
 
   sizeInner = this.sizeTransformation();

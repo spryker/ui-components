61a055af88e453851463ff3b943085fe9035c304
Dmytro Abramov <dmytro.abramov@spryker.com>
2020-03-16 13:05:15 +0200

    refactor(table components): added exports

--- libs/table/columns/src/index.ts
+++ libs/table/columns/src/index.ts
@@ -1,1 +1,6 @@
-export {};
+export * from './table-column-date/table-column-date.module';
+export * from './table-column-date/table-column-date.component';
+export * from './table-column-image/table-column-image.module';
+export * from './table-column-image/table-column-image.component';
+export * from './table-column-text/table-column-text.module';
+export * from './table-column-text/table-column-text.component';

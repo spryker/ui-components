d40ac3f27f64143c9e92bbc727c44f3a5bfddf87
Dmytro Abramov <dmytro.abramov@spryker.com>
2020-03-17 14:25:30 +0200

    refactor(column types): added column types to registry

--- libs/table/src/lib/table/table.ts
+++ libs/table/src/lib/table/table.ts
@@ -2,6 +2,11 @@
 import { TemplateRef, Type } from '@angular/core';
 import { LayoutFlatConfig } from '@orchestrator/layout';
 import { Observable } from 'rxjs';
+import {
+  TableColumnDateConfig,
+  TableColumnTextConfig,
+  TableColumnImageConfig,
+} from '@spryker/table/columns';
 
 export interface TableColumn extends Partial<TableColumnTypeDef> {
   id: string;
@@ -26,6 +31,9 @@ export interface TableColumnTypeOptions {
 export interface TableColumnTypeRegistry {
   // Key is type string - value is type config class
   'layout-flat': LayoutFlatConfig;
+  image: TableColumnImageConfig;
+  text: TableColumnTextConfig;
+  date: TableColumnDateConfig;
 }
 
 export type TableColumnType = keyof TableColumnTypeRegistry;

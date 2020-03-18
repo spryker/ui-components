fc70fda46734c4bc614f34009e14bdeef456282e
Aleksey Belan <alexey.belan@spryker.com>
2020-03-18 12:10:17 +0200

    improvement(styles): add reset

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,4 +1,5 @@
 // Global styles
+import '!style-loader!css-loader!less-loader?javascriptEnabled=true!../libs/styles/src/lib/themes/default/reset.less';
 import '!style-loader!css-loader!less-loader?javascriptEnabled=true!@angular/cdk/overlay-prebuilt.css';
 
 import '@webcomponents/webcomponentsjs/custom-elements-es5-adapter.js';

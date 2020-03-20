8fdee41ccfabdb7efaa7120daacbcfb8eecc7afb
Aleksey Belan <alexey.belan@spryker.com>
2020-03-20 18:43:56 +0200

    improvement(table): fix table stories

--- libs/table/src/lib/table/table.component.stories.ts
+++ libs/table/src/lib/table/table.component.stories.ts
@@ -78,20 +78,12 @@ export const withFeatures = (): IStory => ({
           { id: 'name', sortable: true, title: 'name', width: '20%' },
           { id: 'sku', sortable: true, title: 'sku', width: '20%' },
           { id: 'id3', sortable: true, title: 'id3' },
-          // {
-          //   id: 'sku3',
-          //   type: 'list',
-          //   typeOptions: {
-          //     limit: 2,
-          //     type: 'text',
-          //   },
-          // },
-          // {
-          //   id: 'sku3',
-          //   title: 'sku3 | link',
-          //   type: 'test',
-          //   typeOptions: {text: '${value} in ${row.name}'},
-          // },
+          {
+            id: 'sku3',
+            title: 'sku3 | link',
+            type: 'test',
+            typeOptions: {text: '${value} in ${row.name}'},
+          },
         ],
         selectable: true,
         rowActions: [

46ef73d31be515f0aa900cabae6e7df5625d37fe
Aleksey Belan <alexey.belan@spryker.com>
2020-03-23 13:06:21 +0200

    improvement(utils): fix utils import

--- libs/utils/src/index.ts
+++ libs/utils/src/index.ts
@@ -1,5 +1,6 @@
 export * from './lib/prop-transformation';
-export * from './lib/apply-attrs';
+export * from './lib/apply-attrs/apply-attrs.module';
+export * from './lib/apply-attrs/apply-attrs.directive';
 export * from './lib/of-type';
 export * from './lib/to-json';
 export * from './lib/to-boolean';
--- libs/utils/src/lib/apply-attrs/index.ts
+++
@@ -1,2 +0,0 @@
-export * from './apply-attrs.module';
-export * from './apply-attrs.directive';

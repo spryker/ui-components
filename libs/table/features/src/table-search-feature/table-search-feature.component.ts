00df83f4935ed397ccc9c7b24e4069f8bd7da71b
Dmytro Abramov <dmytro.abramov@spryker.com>
2020-03-30 13:22:01 +0300

    refactor(tabel): fixed types

--- libs/table/features/src/table-search-feature/table-search-feature.component.ts
+++ libs/table/features/src/table-search-feature/table-search-feature.component.ts
@@ -17,9 +17,12 @@ import {
 import { Subject, Observable } from 'rxjs';
 
 declare module '@spryker/table' {
-  interface TableConfig {
-    search?: TableSearchConfig;
-  }
+  // tslint:disable-next-line: no-empty-interface
+  interface TableConfig extends TableConfigWithSearch {}
+}
+
+export interface TableConfigWithSearch {
+  search?: TableSearchConfig;
 }
 
 export interface TableSearchConfig {
@@ -43,7 +46,6 @@ export class TableSearchFeatureComponent extends TableFeatureComponent
   implements OnDestroy, AfterViewInit {
   @Input() location = 'top';
   @Input() styles = { order: '99' };
-  placeholder = '';
   destroyed$ = new Subject();
   setValue$ = new Subject<string>();
   valueChange$ = this.setValue$.pipe(
@@ -52,12 +54,12 @@ export class TableSearchFeatureComponent extends TableFeatureComponent
     distinctUntilChanged(),
     takeUntil(this.destroyed$),
   );
-  placeholderSubject$: Observable<TableSearchConfig> | undefined;
+  placeholder$: Observable<string> | undefined;
 
   ngAfterViewInit(): void {
-    this.placeholderSubject$ = this.table?.config$.pipe(
-      map((config: any) => config.search?.placeholder || this.placeholder),
-    );
+    this.placeholder$ = (this.table?.config$ as Observable<
+      TableConfigWithSearch
+    >).pipe(map(config => config.search?.placeholder || ''));
 
     this.valueChange$.subscribe((value: string) => this.triggerUpdate(value));
   }

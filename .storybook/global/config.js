30281c6aba582b2202ef5b1daf4741db701164be
Alex Malkevich <malkevich.alex@gmail.com>
2020-04-22 03:03:09 +0200

    chore(storybook): migrate to new configuration

--- .storybook/global/config.js
+++
@@ -1,7 +0,0 @@
-import { configure, addDecorator } from '@storybook/angular';
-import { withKnobs } from '@storybook/addon-knobs';
-
-import '../config';
-
-addDecorator(withKnobs);
-configure(require.context('../../libs', true, /\.stories\.tsx?$/), module);
--- libs/locale/.storybook/config.js
+++
@@ -1,6 +0,0 @@
-import { configure, addDecorator } from '@storybook/angular';
-import { withKnobs } from '@storybook/addon-knobs';
-import '../../../.storybook/config';
-
-addDecorator(withKnobs);
-configure(require.context('..', true, /\.stories\.tsx?$/), module);

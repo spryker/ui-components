f29702c1c6af44a61ae52a9b47ed70ca3d713be5
Dmytro Abramov <dmytro.abramov@spryker.com>
2020-04-26 19:26:39 +0300

    fix(table): ui-issues

--- libs/table/features/src/table-search-feature/table-search-feature.component.ts
+++ libs/table/features/src/table-search-feature/table-search-feature.component.ts
@@ -5,15 +5,17 @@ import {
   AfterViewInit,
   Input,
   OnDestroy,
+  ViewChild,
 } from '@angular/core';
 import { TableFeatureComponent, TableFeatureLocation } from '@spryker/table';
+import { InputComponent } from '@spryker/input';
 import {
   debounceTime,
   distinctUntilChanged,
   takeUntil,
-  filter,
   map,
 } from 'rxjs/operators';
+import { IconRemoveModule } from '@spryker/icon/icons';
 import { Subject, Observable } from 'rxjs';
 
 declare module '@spryker/table' {
@@ -44,17 +46,19 @@ export interface TableSearchConfig {
 })
 export class TableSearchFeatureComponent extends TableFeatureComponent
   implements OnDestroy, AfterViewInit {
+  @ViewChild(InputComponent) input?: InputComponent;
   @Input() location = TableFeatureLocation.top;
   @Input() styles = { order: '99' };
   destroyed$ = new Subject();
   setValue$ = new Subject<string>();
   valueChange$ = this.setValue$.pipe(
-    filter(value => value.length > 2 || !value.length),
     debounceTime(300),
     distinctUntilChanged(),
     takeUntil(this.destroyed$),
   );
   placeholder$: Observable<string> | undefined;
+
+  removeIcon = IconRemoveModule.icon;
 
   ngAfterViewInit(): void {
     this.placeholder$ = (this.table?.config$ as Observable<
@@ -77,5 +81,12 @@ export class TableSearchFeatureComponent extends TableFeatureComponent
     this.dataConfiguratorService?.update({
       search: inputValue || '',
     });
+  }
+
+  suffixClickHandler(): void {
+    if (this.input && this.input.value) {
+      this.input.value = null;
+      this.inputValueChange('');
+    }
   }
 }

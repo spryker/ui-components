96a8e6828f845dd5f09c711059cea6e1708dd1f8
Dmytro Abramov <dmytro.abramov@spryker.com>
2020-05-12 17:39:19 +0300

    feat(date-picker): fixed dependency

--- libs/date-picker/src/lib/date-range-picker/date-range-picker.module.ts
+++ libs/date-picker/src/lib/date-range-picker/date-range-picker.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DateRangePickerComponent } from './date-range-picker.component';
-import { DatePickerModule } from '@spryker/date-picker';
+import { DatePickerModule } from '../date-picker.module';
 import { FormsModule } from '@angular/forms';
 
 @NgModule({

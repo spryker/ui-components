1e7083b7c3b0c721d76eaf763ec7cea2f2881d29
Dmytro Abramov <dmytro.abramov@spryker.com>
2020-05-13 17:38:24 +0300

    feat(date-picker): changed type of fuction

--- libs/date-picker/src/lib/date-picker/tokens.ts
+++ libs/date-picker/src/lib/date-picker/tokens.ts
@@ -2,7 +2,9 @@ import { InjectionToken } from '@angular/core';
 
 export type DateWorkDays = number[];
 
-export const getWorkDaysNumbers = (): number[] => [1, 2, 3, 4, 5];
+export function getWorkDaysNumbers(): number[] {
+  return [1, 2, 3, 4, 5];
+}
 
 // Token holds array of number of days in a week and by default includes days from Monday to Friday
 export const DateWorkDaysToken = new InjectionToken<DateWorkDays>(

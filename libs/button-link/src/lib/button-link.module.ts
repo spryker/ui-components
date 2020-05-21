d3e736dab8421cbe2ba177fc06daf8a285b2e635
Dmytro Abramov <dmytro.abramov@spryker.com>
2020-05-21 17:47:34 +0300

    feat(button-link): added component export

--- libs/button-link/src/lib/button-link.module.ts
+++ libs/button-link/src/lib/button-link.module.ts
@@ -5,5 +5,6 @@ import { ButtonLinkComponent } from './button-link/button-link.component';
 @NgModule({
   imports: [CommonModule],
   declarations: [ButtonLinkComponent],
+  exports: [ButtonLinkComponent],
 })
 export class ButtonLinkModule {}

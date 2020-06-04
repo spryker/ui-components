aa462570d1b7a8b4cc8fe286db7ad1802722e88b
Dmytro Abramov <dmytro.abramov@spryker.com>
2020-06-04 14:34:20 +0300

    feat(notification wrapper): added import of types

--- libs/notification/src/index.ts
+++ libs/notification/src/index.ts
@@ -2,3 +2,4 @@ export * from './lib/notification.module';
 export * from './lib/notification/notification.component';
 export * from './lib/notification.service';
 export * from './lib/notification-wrapper/notification-wrapper.component';
+export * from './lib/types';

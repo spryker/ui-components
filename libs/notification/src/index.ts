41a8d203703b1b512edf70c602f3803798eaa847
Dmytro Abramov <dmytro.abramov@spryker.com>
2020-06-04 14:21:11 +0300

    feat(notification wrapper): added imports

--- libs/notification/src/index.ts
+++ libs/notification/src/index.ts
@@ -1,2 +1,4 @@
 export * from './lib/notification.module';
 export * from './lib/notification/notification.component';
+export * from './lib/notification.service';
+export * from './lib/notification-wrapper/notification-wrapper.component';

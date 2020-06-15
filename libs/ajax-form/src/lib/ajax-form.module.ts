3c18d9eef48c0a4bbef8e314e19c881ae560ecbe
Dmytro Abramov <dmytro.abramov@spryker.com>
2020-06-15 17:12:56 +0300

    improvement(ajax-form-loading): removed modules

--- libs/ajax-form/src/lib/ajax-form.module.ts
+++ libs/ajax-form/src/lib/ajax-form.module.ts
@@ -2,17 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AjaxFormComponent } from './ajax-form/ajax-form.component';
 import { StaticHtmlRendererModule } from '@spryker/html-renderer';
-import { HttpClientModule } from '@angular/common/http';
 import { NzSpinModule } from 'ng-zorro-antd/spin';
-import { IconModule } from '@spryker/icon';
 
 @NgModule({
   imports: [
     CommonModule,
     StaticHtmlRendererModule,
-    HttpClientModule,
     NzSpinModule,
-    IconModule,
   ],
   declarations: [AjaxFormComponent],
   exports: [AjaxFormComponent],

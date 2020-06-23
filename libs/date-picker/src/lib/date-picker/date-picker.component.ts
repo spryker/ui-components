3b57ec1f8e551872fa74c532b1fceb021266fa01
Aleksey Belan <alexey.belan@spryker.com>
2020-06-23 19:54:59 +0300

    improvement(date-picker): change static property name

--- libs/date-picker/src/lib/date-picker/date-picker.component.ts
+++ libs/date-picker/src/lib/date-picker/date-picker.component.ts
@@ -43,7 +43,7 @@ export type EnableDateFunction = (current: Date) => boolean;
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class DatePickerComponent implements OnChanges, AfterViewChecked {
-  private static defaultFormat = 'dd.MM.yyyy';
+  private static DefaultFormat = 'dd.MM.yyyy';
 
   @Input() @ToBoolean() clearButton = true;
   @Input() @ToBoolean() disabled = false;
@@ -57,7 +57,7 @@ export class DatePickerComponent implements OnChanges, AfterViewChecked {
   get date(): Date | string {
     return this._date as Date;
   }
-  @Input() format = DatePickerComponent.defaultFormat;
+  @Input() format = DatePickerComponent.DefaultFormat;
   @Input() placeholder?: string;
   @Input() name?: string;
   @Output() dateChange = new EventEmitter<Date>();
@@ -82,7 +82,7 @@ export class DatePickerComponent implements OnChanges, AfterViewChecked {
     this.updatePicker();
 
     if ('format' in changes && !this.format) {
-      this.format = DatePickerComponent.defaultFormat;
+      this.format = DatePickerComponent.DefaultFormat;
     }
 
     if (typeof this.enableDate === 'function') {

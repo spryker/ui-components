3a9766b41756e0665aa3c67c9a03465eb325912b
Aleksey Belan <alexey.belan@spryker.com>
2020-06-24 12:46:55 +0300

    improvement(notification, date-picker) improve styles

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,5 +1,4 @@
 // Global styles
-import '!style-loader!css-loader!less-loader?javascriptEnabled=true!../libs/styles/src/lib/themes/default/reset.less';
-import '!style-loader!css-loader!less-loader?javascriptEnabled=true!@angular/cdk/overlay-prebuilt.css';
+import '!style-loader!css-loader!less-loader?javascriptEnabled=true!./styles.less';
 
 import '@webcomponents/webcomponentsjs/custom-elements-es5-adapter.js';

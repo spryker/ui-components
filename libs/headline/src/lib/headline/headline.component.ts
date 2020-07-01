9e3ea2390b402b7cee25a5eee41f36ef3eac8d8c
Aleksey Belan <alexey.belan@spryker.com>
2020-07-01 12:24:12 +0300

    improvement(headline): changed level input type

--- libs/headline/src/lib/headline/headline.component.ts
+++ libs/headline/src/lib/headline/headline.component.ts
@@ -5,7 +5,14 @@ import {
   Input,
 } from '@angular/core';
 
-export type Level = 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'base';
+export enum Level {
+  H1 = 'h1',
+  H2 = 'h2',
+  H3 = 'h3',
+  H4 = 'h4',
+  H5 = 'h5',
+  Base = 'base',
+}
 
 @Component({
   selector: 'spy-headline',
@@ -18,5 +25,5 @@ export type Level = 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'base';
   },
 })
 export class HeadlineComponent {
-  @Input() level: Level = 'h1';
+  @Input() level: Level = Level.H1;
 }

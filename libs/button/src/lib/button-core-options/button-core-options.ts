80b37bcbe8c730f72880f8c66792bcd9ca0fdede
Aleksey Belan <alexey.belan@spryker.com>
2020-07-03 18:47:49 +0300

    improvement(button): remove class duplication

--- libs/button/src/lib/button-core-options/button-core-options.ts
+++ libs/button/src/lib/button-core-options/button-core-options.ts
@@ -7,9 +7,30 @@ import {
   ButtonVariant,
 } from './types';
 
+export const buttonClassName = 'spy-button';
+
 export class ButtonCoreOptions {
   @Input() shape: ButtonShape = ButtonShape.Default;
   @Input() size: ButtonSize = ButtonSize.Medium;
   @Input() variant: ButtonVariant = ButtonVariant.Primary;
   @Input() @ToJson() attrs?: ButtonAttributes;
+
+  classList?: string;
+
+  ngOnInit(): void {
+    this.setClassList();
+  }
+
+  ngOnChanges(): void {
+    this.setClassList();
+  }
+
+  private setClassList(): void {
+    this.classList = `
+      ${buttonClassName}
+      ${buttonClassName}--${this.shape}
+      ${buttonClassName}--${this.size}
+      ${buttonClassName}--${this.variant}
+    `;
+  }
 }

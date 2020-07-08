5c59c0308c9a995e3abb92c2dcdccb2a03f9ec6a
Dmytro Abramov <abramov@admings-MacBook-Pro.local>
2020-07-08 11:27:15 +0300

    feat(html-overlay): removed code

--- libs/table/action-handlers/src/table-form-overlay-action-handler/table-form-overlay-action-handler.component.ts
+++ libs/table/action-handlers/src/table-form-overlay-action-handler/table-form-overlay-action-handler.component.ts
@@ -6,7 +6,6 @@ import {
 import { DrawerRef } from '@spryker/drawer';
 import { TableFormOverlayOptions } from './types';
 import { Observable } from 'rxjs';
-import { pluck, map } from 'rxjs/operators';
 
 @Component({
   selector: 'spy-table-form-overlay-action-handler',
@@ -20,15 +19,8 @@ import { pluck, map } from 'rxjs/operators';
 })
 export class TableFormOverlayActionHandlerComponent {
   data$ = this.drawerRef.options.data;
-  url$: any;
-  url: any;
 
   constructor(
     private drawerRef: DrawerRef<Observable<TableFormOverlayOptions>>,
-  ) {
-    this.url$ = this.drawerRef.options.data?.subscribe({
-      next: value => console.log(value),
-    });
-    // console.log(this.url$, this.url);
-  }
+  ) {}
 }

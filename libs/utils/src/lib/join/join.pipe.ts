bde4576098a7c7280662fdc242c67bb680f8be02
Dmytro Abramov <abramov.dmitry21@gmail.com>
2020-07-14 11:16:50 +0300

    improvement(select): fixed pipe params

--- libs/utils/src/lib/join/join.pipe.ts
+++ libs/utils/src/lib/join/join.pipe.ts
@@ -4,7 +4,7 @@ import { Pipe, PipeTransform } from '@angular/core';
   name: 'spyJoin',
 })
 export class JoinPipe implements PipeTransform {
-  transform(value: string[], key: string): string | undefined {
-    return value?.join(key);
+  transform(value?: string[], key?: string): string {
+    return value?.join(key) || '';
   }
 }

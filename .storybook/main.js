6ed96c728c0c8e0f7e45c9500a6651916d7d29c8
Alex Malkevich <malkevich.alex@gmail.com>
2020-08-18 14:02:39 +0200

    docs(storybook): add abstract addon

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,8 @@
 module.exports = {
   stories: ['../**/*.stories.@(ts|mdx)'],
-  addons: ['@storybook/addon-knobs', '@storybook/addon-docs'],
+  addons: [
+    '@storybook/addon-knobs',
+    '@storybook/addon-docs',
+    'storybook-addons-abstract',
+  ],
 };

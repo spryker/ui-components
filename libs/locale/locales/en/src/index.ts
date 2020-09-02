6459d7c7dbff7af1d5c989054197a4e15447499b
Aleksey Belan <alexey.belan@spryker.com>
2020-09-02 16:52:03 +0300

    improvement(locale): add date-fns locales

--- libs/locale/locales/en/src/index.ts
+++ libs/locale/locales/en/src/index.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { LocaleRecord, provideLocaleRecordsFactory } from '@spryker/locale';
+import { enUS } from 'date-fns/locale';
 
 export const EN_LOCALE = 'en';
 
@@ -15,8 +16,12 @@ export function enLocaleFactory() {
           data: m.default,
         })),
       ant: () =>
-        import('ng-zorro-antd/esm5/i18n/languages/en_US' as any).then(m => ({
-          data: m.default,
+        Promise.all([
+          import('ng-zorro-antd/esm5/i18n/languages/en_US' as any),
+          import('date-fns/locale/en-US'),
+        ]).then(([data, date]) => ({
+          data: data.default,
+          dateData: date.default,
         })),
       spryker: () =>
         import('@spryker/locale/data/en' as any).then(m => ({

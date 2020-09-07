0d7219da81ffe3283a3b558e692ce71d74e98391
Aleksey Belan <alexey.belan@spryker.com>
2020-09-07 18:52:10 +0300

    ci: fix build

---
+++ libs/interception/src/index.ts
@@ -0,0 +1,5 @@
+export * from './lib/types';
+export * from './lib/interception.module';
+export * from './lib/interception.service';
+export * from './lib/interception-composer.directive';
+export * from './lib/interception-composer.service';

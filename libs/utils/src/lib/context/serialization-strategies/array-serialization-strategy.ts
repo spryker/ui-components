730e35d23d0199896dd2c4706b164bfd249f959b
Aleksey Belan <alexey.belan@spryker.com>
2020-11-27 14:24:55 +0200

    improvement(utils): add nested array serialization

--- libs/utils/src/lib/context/serialization-strategies/array-serialization-strategy.ts
+++ libs/utils/src/lib/context/serialization-strategies/array-serialization-strategy.ts
@@ -1,16 +1,41 @@
 import { Injectable } from '@angular/core';
 import { ContextSerializationStrategy } from '../serialization-strategy';
+import { ContextSerializationService } from '../context-serialization.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ArrayContextSerializationStrategy
   implements ContextSerializationStrategy<Array<any>> {
+  constructor(
+    private contextSerializationService: ContextSerializationService,
+  ) {}
+
   canSerialize(value: unknown): value is Array<any> {
     return Array.isArray(value);
   }
 
   serialize(value: Array<any>): string {
-    return `[${value}]`;
+    const serializedValue = this.recursiveMapping(value);
+
+    return `[${serializedValue}]`;
+  }
+
+  private recursiveMapping(value: unknown) {
+    if (!this.canSerialize(value)) {
+      return this.serialization(value);
+    }
+
+    return value.map((v) => {
+      if (Array.isArray(v)) {
+        this.recursiveMapping(v);
+      }
+
+      return this.serialization(v);
+    });
+  }
+
+  private serialization(value: unknown): string {
+    return this.contextSerializationService.serialize(value);
   }
 }

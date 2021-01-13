88447960fad4add09904c15231b76e122e4f5ad6
Aleksey Belan <alexey.belan@spryker.com>
2021-01-13 12:26:20 +0200

    improvement(web-componenst): fix barrel export

--- libs/web-components/src/index.ts
+++ libs/web-components/src/index.ts
@@ -1,5 +1,5 @@
 export * from './lib/web-components.module';
 export * from './lib/custom-element';
 export * from './lib/ng-web-component';
-export * from './lib/select-components';
+export * from './lib/select-components/index';
 export * from './lib/custom-element-boundary';

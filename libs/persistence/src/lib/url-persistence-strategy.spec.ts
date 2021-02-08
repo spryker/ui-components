d739cd970c3fc334ae5ecbe12878e3b8703da5f9
Aleksey Belan <alexey.belan@spryker.com>
2021-02-08 13:00:15 +0200

    improvement(persistence): add jsDocs and tests

--- libs/persistence/src/lib/url-persistence-strategy.spec.ts
+++ libs/persistence/src/lib/url-persistence-strategy.spec.ts
@@ -1,4 +1,4 @@
-import { fakeAsync, TestBed, tick } from '@angular/core/testing';
+import { TestBed } from '@angular/core/testing';
 import { WindowToken } from '@spryker/utils';
 
 import { UrlPersistenceStrategy } from './url-persistence-strategy';

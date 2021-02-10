78836a800acd603747d5f47e159c26e7f2495119
Alex Malkevich <malkevich.alex@gmail.com>
2021-02-10 18:52:17 +0100

    chore(staged): use NX affected commands directly for staged files

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,3 +1,11 @@
 module.exports = {
-  '*.{jsx?,tsx?,json,s?css,less,html}': ['prettier --write'],
+  '*.{jsx?,tsx?,json,s?css,less,html}': (files) => [
+    `npm run format:write -- --files ${files.join(',')}`,
+  ],
+  '{libs,apps}/**/*.{tsx?,jsx?}': (files) => [
+    `npm run affected:lint -- --fix --files ${files.join(',')}`,
+  ],
+  '{libs,apps}/**/*.{tsx?,jsx?}': (files) => [
+    `npm run affected:test -- --files ${files.join(',')}`,
+  ],
 };

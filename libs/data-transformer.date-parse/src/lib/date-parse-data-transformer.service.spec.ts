718e0c0289c2e3911ddcd7df6c61a84797b0aaa7
Aleksey Belan <alexey.belan@spryker.com>
2021-02-25 01:27:08 +0200

    feat(data-transformer.date-parse): fix module name

--- libs/data-transformer.date-parse/src/lib/date-parse-data-transformer.service.spec.ts
+++ libs/data-transformer.date-parse/src/lib/date-parse-data-transformer.service.spec.ts
@@ -1,13 +1,28 @@
 import { TestBed } from '@angular/core/testing';
 
 import { DateParseDataTransformerService } from './date-parse-data-transformer.service';
+import { DateService } from '@spryker/utils/date';
+
+class MockDateService {
+  parse = jest.fn();
+}
 
 describe('DateParseDataTransformerService', () => {
   let service: DateParseDataTransformerService;
+  let dateService: MockDateService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [
+        MockDateService,
+        {
+          provide: DateService,
+          useExisting: MockDateService,
+        },
+      ],
+    });
     service = TestBed.inject(DateParseDataTransformerService);
+    dateService = TestBed.inject(MockDateService);
   });
 
   it('transform method should returns the date number of milliseconds', () => {
@@ -16,6 +31,9 @@ describe('DateParseDataTransformerService', () => {
     };
     const mockData = '2021-02-15T10:57:07.086Z';
     const callback = jest.fn();
+
+    dateService.parse.mockImplementation((value) => new Date(value));
+
     const transformObservable$ = service.transform(mockData, mockConfig);
     const mockReturnValue = new Date(mockData).getTime();
 

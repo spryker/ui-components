78d0025461ae17699b2ae0ef640ecbe1b31b0996
Yuriy Gerton <yuriy.gerton@spryker.com>
2021-03-29 15:03:35 +0300

    fix(button.icon): rename theme file

--- libs/button.icon/src/index.ts
+++ libs/button.icon/src/index.ts
@@ -1,1 +1,2 @@
 export * from './lib/button-icon.module';
+export * from './lib/button-icon/button-icon.component';
--- libs/button.icon/src/lib/button-icon/button-icon.component.ts
+++ libs/button.icon/src/lib/button-icon/button-icon.component.ts
@@ -28,7 +28,7 @@ export class ButtonIconComponent {
   @ToBoolean()
   disabled = false;
 
-  @ViewChild('buttonRef') buttonRef?: ElementRef<HTMLElement>;
+  @ViewChild('buttonRef', { static: true }) buttonRef?: ElementRef<HTMLElement>;
 
   click() {
     this.buttonRef?.nativeElement.click();

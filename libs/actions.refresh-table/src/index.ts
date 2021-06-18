5abb161d7281ab090c962a7c9d1f56ca686467ea
Aleksey Belan <alexey.belan@spryker.com>
2021-06-18 13:17:58 +0300

    feat(actions.refresh-table): add lib

---
+++ libs/actions.refresh-table/src/index.ts
@@ -0,0 +1,2 @@
+export * from './lib/refresh-table-action-handler.module';
+export * from './lib/refresh-table-action-handler.service';
---
+++ libs/actions.refresh-table/src/lib/refresh-table-action-handler.service.spec.ts
@@ -0,0 +1,68 @@
+import { Injectable } from '@angular/core';
+import { TestBed } from '@angular/core/testing';
+import { TableDataConfiguratorService } from '@spryker/table';
+
+import { RefreshTableActionHandlerService } from './refresh-table-action-handler.service';
+
+const mockActionsConfig = {
+  type: 'redirect',
+  url: 'mockUrl',
+};
+const mockContext = 'mockContext';
+
+@Injectable()
+class MockInjector {
+  get = jest.fn();
+}
+
+@Injectable()
+class MockTableDataConfiguratorService {
+  update = jest.fn();
+}
+
+describe('RefreshTableActionHandlerService', () => {
+  let service: RefreshTableActionHandlerService;
+  let injector: MockInjector;
+  let tableDataConfiguratorService: MockTableDataConfiguratorService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [
+        MockInjector,
+        MockTableDataConfiguratorService,
+        {
+          provide: TableDataConfiguratorService,
+          useExisting: MockTableDataConfiguratorService,
+        },
+      ],
+    });
+
+    service = TestBed.inject(RefreshTableActionHandlerService);
+    injector = TestBed.inject(MockInjector);
+    tableDataConfiguratorService = TestBed.inject(
+      MockTableDataConfiguratorService,
+    );
+
+    injector.get.mockReturnValue(tableDataConfiguratorService);
+  });
+
+  it('should update the table via TableDataConfiguratorService.update()', () => {
+    service.handleAction(injector, mockActionsConfig, mockContext);
+
+    expect(tableDataConfiguratorService.update).toHaveBeenCalledWith({});
+  });
+
+  it('should return stream that emits empty value', () => {
+    const callback = jest.fn();
+
+    const redirectActionService$ = service.handleAction(
+      injector,
+      mockActionsConfig,
+      mockContext,
+    );
+
+    redirectActionService$.subscribe(callback);
+
+    expect(callback).toHaveBeenCalledWith(undefined);
+  });
+});

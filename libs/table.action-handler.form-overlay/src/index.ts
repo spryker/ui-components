04841fc1579390ba2e287ebbcdbfd69f20254d54
Aleksey Belan <alexey.belan@spryker.com>
2021-06-26 21:30:45 +0300

    refactor: remove deprecated libs

--- libs/table.action-handler.form-overlay/src/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './lib/table-form-overlay-action-handler.module';
-export * from './lib/table-form-overlay-action-handler.component';
-export * from './lib/table-form-overlay-action-handler.service';
-export * from './lib/types';
--- libs/table.action-handler.html-overlay/src/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './lib/table-html-overlay-action-handler.service';
-export * from './lib/table-html-overlay-action-handler.module';
-export * from './lib/table-html-overlay-action-handler.component';
-export * from './lib/types';

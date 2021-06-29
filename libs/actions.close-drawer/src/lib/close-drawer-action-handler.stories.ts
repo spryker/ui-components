693b32dc4fc77f70ed834c3edb1b3aa7472b3659
danil.moroz <danil.moroz@spryker.com>
2021-06-29 14:27:26 +0300

    docs(actions.refresh-drawer): fix stories

--- libs/actions.close-drawer/src/lib/close-drawer-action-handler.stories.ts
+++ libs/actions.close-drawer/src/lib/close-drawer-action-handler.stories.ts
@@ -1,62 +1,14 @@
-// import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-// import { object } from '@storybook/addon-knobs';
-// import { ActionsModule } from '@spryker/actions';
-// import { ButtonActionModule } from '@spryker/button.action';
-// import { DrawerModule } from '@spryker/drawer';
-// import { DrawerContainerProxyComponent } from '../../../drawer/src/lib/drawer-container/drawer-container-proxy.component';
-//
-// import { CloseDrawerActionHandlerService } from './close-drawer-action-handler.service';
-//
-// export default {
-//   title: 'CloseDrawerActionHandlerService'
-// }
-//
-// export const primary = () => ({
-//   moduleMetadata: {
-//     imports: [
-//       BrowserAnimationsModule,
-//       ButtonActionModule,
-//       DrawerModule,
-//       ActionsModule.withActions({
-//         close_drawer: CloseDrawerActionHandlerService,
-//       }),
-//     ],
-//     entryComponents: [DrawerContainerProxyComponent],
-//   },
-//   template: `
-//     <spy-button-action
-//       [action]="action"
-//       variant="primary"
-//       size="lg"
-//     >
-//       Close Drawer Via Service
-//     </spy-button-action>
-//
-//     <spy-drawer
-//       [isOpen]="true"
-//     >
-//       <h3>Drawer content</h3>
-//     </spy-drawer>
-//   `,
-//   props: {
-//     action: object('action', {
-//       type: 'close_drawer',
-//     })
-//   }
-// })
-
 import { Component } from '@angular/core';
 import { DrawerModule, DrawerService } from '@spryker/drawer';
-import { NotificationModule } from '@spryker/notification';
-import { ButtonActionComponent, ButtonActionModule } from '@spryker/button.action';
+import { ButtonModule } from '@spryker/button';
+import { ButtonActionModule } from '@spryker/button.action';
 import { ActionsModule } from '@spryker/actions';
 import { WebComponentsModule } from '@spryker/web-components';
 import { IStory } from '@storybook/angular';
 import { object } from '@storybook/addon-knobs';
 
-import { DrawerContainerComponent } from '../../../drawer/src/lib/drawer-container/drawer-container.component';
-import { CloseDrawerActionHandlerService } from './close-drawer-action-handler.service';
 import { DrawerContainerProxyComponent } from '../../../drawer/src/lib/drawer-container/drawer-container-proxy.component';
+import { CloseDrawerActionHandlerService } from './close-drawer-action-handler.service';
 
 export default {
   title: 'CloseDrawerActionHandlerService',
@@ -65,21 +17,15 @@ export default {
 @Component({
   selector: 'spy-story',
   template: `
-    <button (click)="clickHandler()">Toggle drawer</button>
-    <spy-button-action
-      [action]="action"
+    <spy-button
       variant="primary"
-      size="lg"
-    >
-      Close Drawer Via Service
-    </spy-button-action>
+      size="md"
+      (click)="clickHandler()">
+      Open drawer
+    </spy-button>
   `,
 })
 class SimpleDrawerComponent {
-  action = {
-    type: 'close_drawer',
-  }
-
   constructor(private drawerService: DrawerService) {}
 
   clickHandler(): void {
@@ -91,6 +37,14 @@ class SimpleDrawerComponent {
   selector: 'spy-drawer-content',
   template: `
     <h3>Drawer content here...</h3>
+
+    <spy-button-action
+      [action]="{ type: 'close_drawer' }"
+      variant="primary"
+      size="md"
+    >
+      Close Drawer Via Service
+    </spy-button-action>
   `,
 })
 class DrawerContentComponent {}
@@ -100,7 +54,7 @@ export const primary = (): IStory => ({
     imports: [
       WebComponentsModule.forRoot(),
       DrawerModule,
-      NotificationModule.forRoot(),
+      ButtonModule,
       ButtonActionModule,
       ActionsModule.withActions({
         close_drawer: CloseDrawerActionHandlerService,
@@ -108,8 +62,6 @@ export const primary = (): IStory => ({
     ],
     declarations: [SimpleDrawerComponent, DrawerContentComponent],
     entryComponents: [
-      ButtonActionComponent,
-      DrawerContainerComponent,
       DrawerContentComponent,
       DrawerContainerProxyComponent,
     ],
@@ -123,4 +75,3 @@ export const primary = (): IStory => ({
     })
   }
 });
-

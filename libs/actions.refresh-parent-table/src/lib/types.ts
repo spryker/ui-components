8f391e0cff88b7ae138944a16b1a32ffd001af9f
danil.moroz <danil.moroz@spryker.com>
2021-07-08 16:40:51 +0300

    fix(actions.refresh-parent-table): fix action name

--- libs/actions.refresh-parent-table/src/lib/types.ts
+++ libs/actions.refresh-parent-table/src/lib/types.ts
@@ -2,7 +2,7 @@ import { ActionConfig } from '@spryker/actions';
 
 declare module '@spryker/actions' {
   interface ActionsRegistry {
-    refresh_parent_table: RefreshParentTableActionConfig;
+    'refresh-parent-table': RefreshParentTableActionConfig;
   }
 }
 

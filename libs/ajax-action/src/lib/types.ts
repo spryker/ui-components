f97301b8510886a2172176023472ca57dae8fa52
Aleksey Belan <alexey.belan@spryker.com>
2021-07-09 15:44:36 +0300

    fix(ajax-action): remove types

--- libs/ajax-action/src/lib/types.ts
+++ libs/ajax-action/src/lib/types.ts
@@ -1,24 +1,4 @@
-import { Type, Injector } from '@angular/core';
 import { ActionConfig } from '@spryker/actions';
-
-// tslint:disable-next-line: no-empty-interface
-export interface AjaxPostActionRegistry {}
-
-export type AjaxPostActionType = keyof AjaxPostActionRegistry;
-
-export interface AjaxPostAction {
-  type: string;
-  // Specific AjaxPostAction types may have custom props
-  [k: string]: unknown;
-}
-
-export interface AjaxPostActionHandler {
-  handleAction(action: AjaxPostAction, injector: Injector): void;
-}
-
-export interface AjaxPostActionsDeclaration {
-  [type: string]: Type<AjaxPostActionHandler>;
-}
 
 export interface AjaxActionResponse {
   actions?: ActionConfig[];

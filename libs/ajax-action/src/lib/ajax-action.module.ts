807c49859377515ef1c21e224c9f4a3d27691e93
Aleksey Belan <alexey.belan@spryker.com>
2021-07-09 15:32:53 +0300

    fix(ajax-action): fix module

--- libs/ajax-action/src/lib/ajax-action.module.ts
+++ libs/ajax-action/src/lib/ajax-action.module.ts
@@ -1,8 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { NotificationModule } from '@spryker/notification';
 
 @NgModule({
-  imports: [CommonModule, NotificationModule],
+  imports: [CommonModule],
 })
 export class AjaxActionModule {}

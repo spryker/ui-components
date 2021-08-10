6d8d643a159e513214d275b2994849f42d00df93
danil.moroz <danil.moroz@spryker.com>
2021-08-10 20:49:40 +0300

    fix(table): add error message to `table-locator` if `tableId` is undefined

--- libs/table/src/lib/table-locator/internal-table-locator.service.ts
+++ libs/table/src/lib/table-locator/internal-table-locator.service.ts
@@ -7,7 +7,7 @@ import { TableComponent } from '../table';
   providedIn: 'root',
 })
 export class InternalTableLocatorService implements TableLocatorService {
-  private tableIdsMap = new Map<string | undefined, TableComponent>();
+  private tableIdsMap = new Map<string, TableComponent>();
 
   findById(id: string): TableComponent | undefined {
     return this.tableIdsMap.get(id);
@@ -16,6 +16,12 @@ export class InternalTableLocatorService implements TableLocatorService {
   register(table: TableComponent): Observable<void> {
     return new Observable<void>(() => {
       const tableId = table.tableId;
+
+      if (!tableId) {
+        throw new Error(
+          `InternalTableLocatorService: Table Id is not defined!`,
+        );
+      }
 
       this.tableIdsMap.set(tableId, table);
 

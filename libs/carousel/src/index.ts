5aa1b08528265fbf3270dc8d9c6a8822ba03fd3b
Rustem Zakiev <temrhythm@icloud.com>
2021-09-06 17:04:12 +0700

    Revert "feat(carousel): added main functionality of carousel"
    This reverts commit 4f5ddbf752b97663599e09eae02953f06884e248.

--- libs/carousel/src/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './lib/carousel.module';
-export * from './lib/types';
-export * from './lib/carousel/carousel.component';
-export * from './lib/carousel-slide/carousel-slide.component';

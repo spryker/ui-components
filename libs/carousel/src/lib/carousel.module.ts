dc01add4b7fa450149ff81abdc9d04c03d3f63e2
Rustem Zakiev <temrhythm@icloud.com>
2021-09-06 19:04:46 +0700

    Revert "Revert "feat(carousel): added main functionality of carousel""
    This reverts commit 5aa1b08528265fbf3270dc8d9c6a8822ba03fd3b.

---
+++ libs/carousel/src/lib/carousel.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CarouselComponent } from './carousel/carousel.component';
+import { CarouselSlideComponent } from './carousel-slide/carousel-slide.component';
+import { SwiperModule } from 'swiper/angular';
+import { IconModule } from '@spryker/icon';
+import { IconPaginationArrowModule } from '@spryker/icon/icons';
+
+@NgModule({
+  imports: [CommonModule, SwiperModule, IconModule, IconPaginationArrowModule],
+  declarations: [CarouselComponent, CarouselSlideComponent],
+  exports: [CarouselComponent, CarouselSlideComponent],
+})
+export class CarouselModule {}

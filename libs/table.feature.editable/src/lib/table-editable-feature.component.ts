f165e36a001e877ba29746a916a05beef3ca7181
denis <polit.den63@gmail.com>
2021-09-28 16:23:03 +0300

    fix(table.feature.editable): adjusted `table-editable-feature` component by removing button properties

--- libs/table.feature.editable/src/lib/table-editable-feature.component.ts
+++ libs/table.feature.editable/src/lib/table-editable-feature.component.ts
@@ -12,12 +12,6 @@ import {
   ViewEncapsulation,
 } from '@angular/core';
 import { AjaxActionService } from '@spryker/ajax-action';
-import {
-  ButtonShape,
-  ButtonSize,
-  ButtonType,
-  ButtonVariant,
-} from '@spryker/button';
 import { DataSerializerService } from '@spryker/data-serializer';
 import {
   IconEditModule,
@@ -106,10 +100,6 @@ export class TableEditableFeatureComponent
   warningIcon = IconWarningModule.icon;
   addRowIcon = IconPlusModule.icon;
   tableFeatureLocation = TableFeatureLocation;
-  buttonSize = ButtonSize;
-  buttonVariant = ButtonVariant;
-  buttonShape = ButtonShape;
-  buttonType = ButtonType;
   syncInput: TableDataRow[] = [];
   stringifiedSyncInput?: string;
   url?: TableEditableConfigUrl;

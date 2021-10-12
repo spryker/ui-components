4e87a996efee4674338ead0a6314a834b36679ce
danil.moroz <danil.moroz@spryker.com>
2021-10-12 17:33:11 +0300

    fix(layout): changed imports order

--- libs/layout/src/lib/layout.module.ts
+++ libs/layout/src/lib/layout.module.ts
@@ -1,7 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { ApplyContextsModule } from '@spryker/utils';
 import { CustomElementBoundaryModule } from '@spryker/web-components';
+import { ApplyContextsModule } from '@spryker/utils';
 import { NzLayoutModule } from 'ng-zorro-antd/layout';
 
 import { LayoutComponent } from './layout/layout.component';

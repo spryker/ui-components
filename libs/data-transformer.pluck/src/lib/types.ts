a02cba89303a8acc3972c1fce47e14d3aff58f8a
danil.moroz <danil.moroz@spryker.com>
2021-11-30 17:35:20 +0200

    fix(data-transformer.pluck): removed module declaration

--- libs/data-transformer.pluck/src/lib/types.ts
+++ libs/data-transformer.pluck/src/lib/types.ts
@@ -1,13 +1,5 @@
 import { DataTransformerConfig } from '@spryker/data-transformer';
 
-import { PluckDataTransformerService } from './pluck-data-transformer.service';
-
-declare module '@spryker/data-transformer' {
-  interface DataTransformerRegistry {
-    pluck: PluckDataTransformerService;
-  }
-}
-
 export type PluckDataTransformerData = object;
 export type PluckDataTransformerDataT = unknown;
 

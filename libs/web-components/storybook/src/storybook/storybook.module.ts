d742662caccc26e5a9aefc5bdb247393890fcdd9
Yuriy Gerton <yuriy.gerton@spryker.com>
2022-02-11 18:36:13 +0200

    docs: introduce the storybook component to correctly render web components

---
+++ libs/web-components/storybook/src/storybook/storybook.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { WebComponentsModule } from "../../../src";
+import { StorybookComponent } from './storybook.component';
+
+@NgModule({
+  imports: [
+    WebComponentsModule.forRoot(),
+    WebComponentsModule.withComponents([
+      {
+        component: StorybookComponent,
+        isRoot: true,
+      },
+    ]),
+  ],
+  exports: [StorybookComponent],
+  declarations: [StorybookComponent],
+})
+export class StorybookModule {}

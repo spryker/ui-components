b9e861253eedaa6a846319c00e79cfe573f4cfce
Yuriy Gerton <yuriy.gerton@spryker.com>
2022-07-01 12:21:49 +0300

    docs: fix storybook addons functionality

--- libs/collapsible/src/lib/collapsible/collapsible.component.stories.ts
+++ libs/collapsible/src/lib/collapsible/collapsible.component.stories.ts
@@ -57,7 +57,11 @@ export const withTemplate = (args) => ({
     declarations: [StoryComponent],
   },
   template: `
-    <spy-collapsible [spyTitle]="spyTitle" [alwaysRender]="alwaysRender">
+    <spy-collapsible
+      [spyTitle]="spyTitle"
+      [active]="active"
+      [disabled]="disabled"
+      [alwaysRender]="alwaysRender">
       <ng-template>
         <spy-story></spy-story>
       </ng-template>

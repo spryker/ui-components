f231844114bded6c986031398661f5e11a716e51
danil.moroz <danil.moroz@spryker.com>
2023-01-31 14:13:16 +0200

    fix(utils): fixed timezone offset in the `to-iso-date-format` pipe

--- libs/utils/src/lib/to-iso-date-format/to-iso-date-format-function.ts
+++ libs/utils/src/lib/to-iso-date-format/to-iso-date-format-function.ts
@@ -1,3 +1,7 @@
 export function toIsoDateFormat(date: string | Date): string {
-    return date instanceof Date ? date.toISOString() : new Date(date).toISOString();
+    return date instanceof Date ? localIsoTime(date) : localIsoTime(new Date(date));
+}
+
+function localIsoTime(date: Date): string {
+    return new Date(date.getTime() - date.getTimezoneOffset() * 60000).toISOString();
 }

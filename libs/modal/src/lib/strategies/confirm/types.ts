0af3e735aec77581b7dd9cd44ef6de1c5c83f594
Serhii-SPRYKER <97897069+Serhii-SPRYKER@users.noreply.github.com>
2023-02-01 14:11:29 +0100

    feat: add okButtonVariant to signature (#534)
    * feat: add okButtonVariant to signature
    
    * feat: add button variant to template
    
    * chore(ci): update travis nodejs version

--- libs/modal/src/lib/strategies/confirm/types.ts
+++ libs/modal/src/lib/strategies/confirm/types.ts
@@ -1,5 +1,6 @@
 import { TemplateRef } from '@angular/core';
 import { ModalTemplateContext, AnyModal } from '../../types';
+import { ButtonVariant } from '@spryker/button';
 
 export interface ConfirmModalStrategyOptions extends ConfirmModalData {}
 
@@ -9,6 +10,7 @@ export interface ConfirmModalData {
   icon?: string | TemplateRef<ModalTemplateContext<AnyModal>>;
   okText?: string | TemplateRef<ModalTemplateContext<AnyModal>>;
   okType?: string;
+  okButtonVariant?: ButtonVariant;
   cancelText?: string | TemplateRef<ModalTemplateContext<AnyModal>>;
   cancelType?: string;
   class?: string;

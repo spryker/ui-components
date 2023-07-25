9ac1907f5f7f4b0cd8c6044d8d81b50c0245fc77
danil.moroz <danil.moroz@spryker.com>
2023-07-25 16:33:20 +0300

    test(datasource.trigger.input): add unit tests

--- libs/datasource.trigger.input/src/lib/input-datasource-trigger.service.spec.ts
+++ libs/datasource.trigger.input/src/lib/input-datasource-trigger.service.spec.ts
@@ -1,16 +1,50 @@
+import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { TestBed } from '@angular/core/testing';
-
+import { of } from 'rxjs';
 import { InputDatasourceTriggerService } from './input-datasource-trigger.service';
 
+const mockValue = 'mockValue';
+const mockConfig = {
+    type: 'trigger',
+    event: 'input',
+    datasource: {
+        type: 'test',
+    },
+};
+const callback = jest.fn();
+
+class MockInputDatasourceTrigger {
+    subscribeToEvent = jest.fn();
+}
+
 describe('InputDatasourceTriggerService', () => {
-    let service: InputDatasourceTriggerService;
+    let service: MockInputDatasourceTrigger;
 
     beforeEach(() => {
-        TestBed.configureTestingModule({});
-        service = TestBed.inject(InputDatasourceTriggerService);
+        TestBed.configureTestingModule({
+            providers: [
+                MockInputDatasourceTrigger,
+                {
+                    provide: InputDatasourceTriggerService,
+                    useExisting: MockInputDatasourceTrigger
+                }
+            ],
+            schemas: [NO_ERRORS_SCHEMA],
+            teardown: { destroyAfterEach: false },
+        });
+
+        service = TestBed.inject(MockInputDatasourceTrigger);
     });
 
-    it('should be created', () => {
-        expect(service).toBeTruthy();
+    it('should return trigger element value from `subscribeToEvent()` method', () => {
+        const triggerElement = document.createElement('input');
+
+        service.subscribeToEvent.mockReturnValue(of(mockValue))
+
+        const serviceObservable$ = service.subscribeToEvent(mockConfig, triggerElement);
+
+        serviceObservable$.subscribe(callback);
+
+        expect(callback).toHaveBeenCalledWith(mockValue);
     });
 });

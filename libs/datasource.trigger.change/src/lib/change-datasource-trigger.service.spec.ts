9efc6db0b3df36a577bf119d373ee4045d56031c
danil.moroz <danil.moroz@spryker.com>
2023-07-25 16:37:12 +0300

    test(datasource.trigger.change): add unit tests

--- libs/datasource.trigger.change/src/lib/change-datasource-trigger.service.spec.ts
+++ libs/datasource.trigger.change/src/lib/change-datasource-trigger.service.spec.ts
@@ -1,16 +1,50 @@
+import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { TestBed } from '@angular/core/testing';
-
+import { of } from 'rxjs';
 import { ChangeDatasourceTriggerService } from './change-datasource-trigger.service';
 
+const mockValue = 'mockValue';
+const mockConfig = {
+    type: 'trigger',
+    event: 'change',
+    datasource: {
+        type: 'test',
+    },
+};
+const callback = jest.fn();
+
+class MockChangeDatasourceTrigger {
+    subscribeToEvent = jest.fn();
+}
+
 describe('ChangeDatasourceTriggerService', () => {
-    let service: ChangeDatasourceTriggerService;
+    let service: MockChangeDatasourceTrigger;
 
     beforeEach(() => {
-        TestBed.configureTestingModule({});
-        service = TestBed.inject(ChangeDatasourceTriggerService);
+        TestBed.configureTestingModule({
+            providers: [
+                MockChangeDatasourceTrigger,
+                {
+                    provide: ChangeDatasourceTriggerService,
+                    useExisting: MockChangeDatasourceTrigger
+                }
+            ],
+            schemas: [NO_ERRORS_SCHEMA],
+            teardown: { destroyAfterEach: false },
+        });
+
+        service = TestBed.inject(MockChangeDatasourceTrigger);
     });
 
-    it('should be created', () => {
-        expect(service).toBeTruthy();
+    it('should return trigger element value from `subscribeToEvent()` method', () => {
+        const triggerElement = document.createElement('textarea');
+
+        service.subscribeToEvent.mockReturnValue(of(mockValue));
+
+        const serviceObservable$ = service.subscribeToEvent(mockConfig, triggerElement);
+
+        serviceObservable$.subscribe(callback);
+
+        expect(callback).toHaveBeenCalledWith(mockValue);
     });
 });

a03ff9c2d965501bbf85de6513716a3d2089b697
danil.moroz <danil.moroz@spryker.com>
2023-08-11 16:01:51 +0300

    fix(datasource.dependable): make compatible with web-components

--- libs/datasource.dependable/src/lib/datasource-dependable.component.ts
+++ libs/datasource.dependable/src/lib/datasource-dependable.component.ts
@@ -5,24 +5,50 @@ import {
     ContentChild,
     HostBinding,
     Input,
+    OnDestroy,
     ViewEncapsulation,
 } from '@angular/core';
+import { ReplaySubject, Subject, takeUntil } from 'rxjs';
 import { DatasourceDependableElementsService } from './datasource-dependable-elements.service';
 import { DatasourceDependableElement } from './types';
 
 @Component({
     selector: 'spy-datasource-dependable',
-    template: '<ng-content></ng-content>',
+    template: `
+        <span [spySelectComponents]="dependableElementRef" (spySelectComponentsFound)="dependableElementsFound($event)">
+            <ng-content></ng-content>
+        </span>
+    `,
     encapsulation: ViewEncapsulation.None,
     changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class DatasourceDependableComponent implements AfterContentInit {
-    @ContentChild(DatasourceDependableElement) dependableElement!: DatasourceDependableElement;
+export class DatasourceDependableComponent implements AfterContentInit, OnDestroy {
     @Input() @HostBinding('attr.id') id = '';
+
+    @ContentChild(DatasourceDependableElement)
+    set dependableElement(dependableElement: DatasourceDependableElement) {
+        this.dependableElement$.next(dependableElement);
+    }
+
+    dependableElementRef = DatasourceDependableElement;
+    dependableElement$ = new ReplaySubject<DatasourceDependableElement>(1);
+    destroyed$ = new Subject<void>();
 
     constructor(private datasourceDependableElementsService: DatasourceDependableElementsService) {}
 
     ngAfterContentInit(): void {
-        this.datasourceDependableElementsService.setElement({ [this.id]: this.dependableElement });
+        this.dependableElement$.pipe(takeUntil(this.destroyed$)).subscribe((dependableElement) => {
+            this.datasourceDependableElementsService.setElement({ [this.id]: dependableElement });
+        });
+    }
+
+    ngOnDestroy(): void {
+        this.destroyed$.next();
+    }
+
+    dependableElementsFound(dependableElements: DatasourceDependableElement[]): void {
+        if (dependableElements) {
+            this.dependableElement$.next(dependableElements[0]);
+        }
     }
 }
--- libs/datasource.dependable/src/lib/datasource-dependable.module.ts
+++ libs/datasource.dependable/src/lib/datasource-dependable.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SelectComponentsModule } from '@spryker/web-components';
 import { DatasourceDependableComponent } from './datasource-dependable.component';
 
 @NgModule({
-    imports: [CommonModule],
+    imports: [CommonModule, SelectComponentsModule],
     declarations: [DatasourceDependableComponent],
     exports: [DatasourceDependableComponent],
 })

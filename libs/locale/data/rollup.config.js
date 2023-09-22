1133d9e1de6a543e1d280d5dfa81d1b9de0bc87d
danil.moroz <danil.moroz@spryker.com>
2023-09-22 18:30:32 +0300

    fix(locale): adjust `rollup.config.js` by changing `globby` import to dynamic import

--- libs/locale/data/rollup.config.js
+++ libs/locale/data/rollup.config.js
@@ -1,33 +1,41 @@
 import typescript from '@rollup/plugin-typescript';
 import nodeResolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
-
 import path from 'path';
-import globby from 'globby';
+const globby = import('globby');
 
 const ROOT_DIR = path.resolve(__dirname, '../..');
 const ROOT_GAP = path.relative(process.cwd(), ROOT_DIR);
 
-const locales = globby.sync('*', {
-    cwd: __dirname,
-    deep: 1,
-    onlyDirectories: true,
-});
-
-const localeFiles = locales.reduce(
-    (acc, locale) => ({
-        ...acc,
-        [locale]: globby.sync(`*/src/i18n/${locale}.ts`, {
-            cwd: ROOT_DIR,
-        }),
+const locales = globby.then(({ globbySync }) =>
+    globbySync('*', {
+        cwd: __dirname,
+        deep: 1,
+        onlyDirectories: true,
     }),
-    {},
 );
 
-const localeInputs = Object.fromEntries(
-    Object.entries(localeFiles).map(([locale, files]) => [
-        locale,
-        files.reduce(
+const localeFiles = locales.then((locales) =>
+    locales.reduce(
+        (acc, locale) => ({
+            ...acc,
+            [locale]: globby.then(({ globbySync }) =>
+                globbySync(`*/src/i18n/${locale}.ts`, {
+                    cwd: ROOT_DIR,
+                }),
+            ),
+        }),
+        {},
+    ),
+);
+
+const localeInputs = async () => {
+    const getLocaleFiles = await localeFiles;
+    const entries = Object.entries(getLocaleFiles);
+    const promises = entries.map(async ([locale, files]) => {
+        const getFiles = await files;
+
+        const reducedFiles = getFiles.reduce(
             (acc, file) => ({
                 ...acc,
                 [getPackageName(file)]: path.join(ROOT_GAP, file),
@@ -36,33 +44,55 @@ const localeInputs = Object.fromEntries(
                 empty1: path.join(__dirname, 'empty1.ts'),
                 empty2: path.join(__dirname, 'empty2.ts'),
             },
-        ),
-    ]),
-);
+        );
 
-module.exports = locales
-    .filter((locale) => localeFiles[locale].length)
-    .map((locale) => ({
-        input: localeInputs[locale],
-        output: {
-            dir: path.resolve(__dirname, locale, 'src/data'),
-            format: 'es',
-            chunkFileNames: '[name].ts',
-        },
-        manualChunks: () => 'data',
-        plugins: [
-            nodeResolve(),
-            commonjs(),
-            typescript({
-                types: ['node', 'jest'],
-                target: 'es2015',
-                module: 'esnext',
-                exclude: ['**/*.spec.ts', '**/*.stories.ts'],
-                tsconfig: './tsconfig.base.json',
-            }),
-        ],
-    }));
+        return [locale, reducedFiles];
+    });
+    const resultArray = await Promise.all(promises);
 
-function getPackageName(path) {
+    return resultArray.reduce((acc, [locale, files]) => {
+        acc[locale] = files;
+
+        return acc;
+    }, {});
+};
+
+const generateChunks = async () => {
+    const getLocaleFiles = await localeFiles;
+    const getLocaleInputs = await localeInputs();
+
+    return locales.then((locales) =>
+        locales
+            .filter(async (locale) => {
+                const getLocale = await getLocaleFiles[locale];
+
+                return getLocale.length;
+            })
+            .map((locale) => ({
+                input: getLocaleInputs[locale],
+                output: {
+                    dir: path.resolve(__dirname, locale, 'src/data'),
+                    format: 'es',
+                    chunkFileNames: '[name].ts',
+                },
+                manualChunks: () => 'data',
+                plugins: [
+                    nodeResolve(),
+                    commonjs(),
+                    typescript({
+                        types: ['node', 'jest'],
+                        target: 'es2015',
+                        module: 'esnext',
+                        exclude: ['**/*.spec.ts', '**/*.stories.ts'],
+                        tsconfig: './tsconfig.base.json',
+                    }),
+                ],
+            })),
+    );
+};
+
+const getPackageName = (path) => {
     return path.split('/')[0];
-}
+};
+
+module.exports = () => generateChunks();

7ea08365de3e8e33f396596da7a7663686309ced
Aleksey Belan <alexey.belan@spryker.com>
2024-03-14 22:19:13 +0200

    sb

--- libs/locale/data/rollup.config.mjs
+++ libs/locale/data/rollup.config.mjs
@@ -1,9 +1,9 @@
-import typescript from '@rollup/plugin-typescript';
-import nodeResolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
-import { fileURLToPath } from 'url';
+import nodeResolve from '@rollup/plugin-node-resolve';
+import typescript from '@rollup/plugin-typescript';
 import { globbySync } from 'globby';
 import path from 'path';
+import { fileURLToPath } from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -51,14 +51,11 @@ export default locales
             format: 'es',
             chunkFileNames: '[name].ts',
         },
-        manualChunks: () => 'data',
         plugins: [
             nodeResolve(),
             commonjs(),
             typescript({
                 types: ['node', 'jest'],
-                target: 'es2015',
-                module: 'esnext',
                 exclude: ['**/*.spec.ts', '**/*.stories.ts'],
                 tsconfig: './tsconfig.base.json',
             }),

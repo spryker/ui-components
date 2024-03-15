7dc6949fb754c6e2cb9218db6826e5e4eb61f516
Aleksey Belan <alexey.belan@spryker.com>
2024-03-15 13:45:05 +0200

    deploy script

--- tools/scripts/build-libs.js
+++ tools/scripts/build-libs.js
@@ -1,7 +1,8 @@
 const { execSync } = require('child_process');
+const { resolve } = require('path');
 
 const IGNORE_TAGS = ['type:meta'];
-const projects = require('../../angular.json').projects;
+const ROOT_DIR = resolve(__dirname, '../..');
 
 /**
  * Spawns a command with all buildable projects from `angular.json`
@@ -13,12 +14,24 @@ const projects = require('../../angular.json').projects;
  * @param {string[]} extraArgs Extra args to pass to the NX build command
  */
 async function main(extraArgs) {
+    const { globbySync } = await import('globby');
+    const projectsJson = globbySync('libs/**/project.json', {
+        cwd: ROOT_DIR,
+        expandDirectories: {
+            files: ['project'],
+            extensions: ['json'],
+        },
+    });
+    const projects = await Promise.all(
+        projectsJson.map((path) =>
+            import(resolve(ROOT_DIR, path), { assert: { type: 'json' } }).then((data) => data.default),
+        ),
+    );
+
     /** @type {Record<string, boolean>} */
     const ignoreMap = IGNORE_TAGS.reduce((acc, tag) => ({ ...acc, [tag]: true }), {});
 
-    const projectsToBuild = Object.entries(projects)
-        .filter(([_, p]) => p.tags && p.tags.every((tag) => !ignoreMap[tag]))
-        .map(([name]) => name);
+    const projectsToBuild = projects.filter((p) => p.tags && p.tags.every((tag) => !ignoreMap[tag])).map((p) => p.name);
 
     console.log(`Building projects: ${projectsToBuild.join(', ')}...`);
 

f4b35d340097eaa9e9692c9bbe0dbf3cbc7cbee7
Oleksii Bilan <alexey.belan@spryker.com>
2024-10-21 16:58:22 +0300

    feat: add dependable functionality for inline datasource (#632)

--- libs/datasource.inline/src/lib/datasource-inline.service.ts
+++ libs/datasource.inline/src/lib/datasource-inline.service.ts
@@ -2,13 +2,23 @@ import { Injectable, Injector } from '@angular/core';
 import { Datasource } from '@spryker/datasource';
 import { Observable, of } from 'rxjs';
 
-import { DatasourceInlineConfig } from './types';
+import { DatasourceInlineConfig, DependableDatasourceInlineContext } from './types';
 
 @Injectable({
     providedIn: 'root',
 })
 export class DatasourceInlineService implements Datasource {
-    resolve(injector: Injector, config: DatasourceInlineConfig): Observable<unknown> {
-        return of(config.data);
+    resolve(
+        injector: Injector,
+        config: DatasourceInlineConfig,
+        context?: DependableDatasourceInlineContext,
+    ): Observable<unknown> {
+        if (!config.dependsOnContext) {
+            return of(config.data);
+        }
+
+        const key = context?.[config.dependsOnContext.contextKey];
+
+        return of(config.data[key] ?? config.dependsOnContext.default);
     }
 }

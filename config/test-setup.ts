2abd7002ec9fb9c07b51df16d36a93dd49bbe98f
Aleksey Belan <alexey.belan@spryker.com>
2025-09-14 23:06:40 +0300

    chore: ci

--- config/test-setup.ts
+++ config/test-setup.ts
@@ -1,8 +1,1 @@
-import { setOutputMock } from '@orchestrator/ngx-testing';
 import 'reflect-metadata/lite';
-
-setOutputMock(() => jest.fn());
-
-declare module '@orchestrator/ngx-testing' {
-    interface OutputMock<T = any> extends jest.Mock<void, [T]> {}
-}

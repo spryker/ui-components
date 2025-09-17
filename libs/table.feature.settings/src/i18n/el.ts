195ba0cfee87f5d8e8b0ad955ecc0bb55f41f33e
Maksym Fedets <maksym.fedets@spryker.com>
2025-09-17 11:29:33 +0300

    feat: add greek locale

---
+++ libs/table.feature.settings/src/i18n/el.ts
@@ -0,0 +1,9 @@
+import { I18nLocaleDataPackage } from '@spryker/locale';
+
+export default {
+    name: 'table.feature.settings',
+    data: {
+        title: 'Ρυθμίσεις πίνακα',
+        reset: 'Επαναφορά',
+    },
+} as I18nLocaleDataPackage;
---
+++ libs/table.feature.total/src/i18n/el.ts
@@ -0,0 +1,9 @@
+import { I18nLocaleDataPackage } from '@spryker/locale';
+
+export default {
+    name: 'table.feature.total',
+    data: {
+        'selected:number': '{{ number }} επιλεγμένα',
+        'total:number': '{{ number }} Αποτέλεσμα(τα)',
+    },
+} as I18nLocaleDataPackage;

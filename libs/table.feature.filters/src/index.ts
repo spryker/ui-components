870881fa2959d80d2d05cd818979084e838ca86b
Maksym Fedets <maksym.fedets@spryker.com>
2025-09-19 16:36:30 +0300

    merge with master

--- libs/table.feature.filters/src/index.ts
+++ libs/table.feature.filters/src/index.ts
@@ -2,4 +2,3 @@ export * from './lib/types';
 export * from './lib/tokens';
 export * from './lib/table-filters-feature.component';
 export * from './lib/table-filters-feature.module';
-export * from './lib/tokens';

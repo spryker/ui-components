c52a4b5b17510fed55ad09416d4ecb4f7fe017d5
Aleksey Belan <alexey.belan@spryker.com>
2025-09-20 10:02:51 +0300

    chore: fix build

--- libs/internal-utils/src/lib/create-component-wrapper.ts
+++
@@ -1,7 +0,0 @@
-import { ComponentInputs } from '@orchestrator/ngx-testing';
-
-export const createComponentWrapper = <T extends (...args: any) => any>(
-    createComponent: T,
-    inputs?: ComponentInputs<any>,
-    detectChanges = true,
-): ReturnType<T> => createComponent(inputs, detectChanges);
